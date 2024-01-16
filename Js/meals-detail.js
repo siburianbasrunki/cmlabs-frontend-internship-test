0683519e76cf11b2d0ba6b5b1c5d52d69a142fdd
siburianbasrunki <basrunkisiburian@gmail.com>
2024-01-16 13:37:44 +0700

    finish

--- Js/meals-detail.js
+++ Js/meals-detail.js
@@ -1,40 +1,52 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const breadcrumbContainer = document.getElementById("breadcrumb-container");
-    const mealDetailContainer = document.getElementById("meal-detail-container");
+document.addEventListener("DOMContentLoaded", function () {
+  const breadcrumbContainer = document.getElementById("breadcrumb-container");
+  const mealDetailContainer = document.getElementById("meal-detail-container");
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const mealId = urlParams.get("meal-id");
+  const urlParams = new URLSearchParams(window.location.search);
+  const mealId = urlParams.get("meal-id");
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-        .then(response => response.json())
-        .then(data => {
-            const meal = data.meals[0];
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const meal = data.meals[0];
 
-            const ingredients = [];
-            for (let i = 1; i <= 20; i++) {
-                if (meal[`strIngredient${i}`]) {
-                    ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
-                }
-            }
+      const ingredients = [];
+      for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+          ingredients.push(
+            `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+          );
+        }
+      }
 
-            breadcrumbContainer.innerHTML = `
+      breadcrumbContainer.innerHTML = `
                 <a href="index.html">Home</a> > 
                 <a href="category-detail.html?category-name=${meal.strCategory}">${meal.strCategory}</a> > ${meal.strMeal}
             `;
 
-            mealDetailContainer.innerHTML = `
-                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                <h2>${meal.strMeal}</h2>
-                <p>${meal.strInstructions}</p>
+      mealDetailContainer.innerHTML = `
+            <div class="main-meal">
+                <div>
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                </div>
+                <div>
+                    <h2>${meal.strMeal}</h2>
+                    <p>${meal.strInstructions}</p>
+                </div>
+            </div>
                 <h3>Ingredients:</h3>
                 <ul>
-                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                    ${ingredients
+                      .map((ingredient) => `<li>${ingredient}</li>`)
+                      .join("")}
                 </ul>
                 <div>
                     <h3>Youtube Tutorial:</h3>
-                    <iframe width="560" height="315" src="${meal.strYoutube}" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="${
+                      meal.strYoutube
+                    }" frameborder="0" allowfullscreen></iframe>
                 </div>
             `;
-        })
-        .catch(error => console.error("Error fetching meal details:", error));
+    })
+    .catch((error) => console.error("Error fetching meal details:", error));
 });
